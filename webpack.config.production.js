789d3fc19d3921cb3c60695927e01c5a6d7bad50
Jeffrey Klein <jeffwklein@gmail.com>
2016-09-10 16:55:07 -0500

    update boilerplate

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,47 +1,78 @@
-'use strict';
+/**
+ * WEBPACK CONFIG
+ *
+ * Notes on config properties:
+ *
+ * 'entry'
+ * Entry point for the bundle.
+ *
+ * 'output'
+ * If you pass an array - the modules are loaded on startup. The last one is exported.
+ *
+ * 'resolve'
+ * Array of file extensions used to resolve modules.
+ *
+ * devtool: 'eval-source-map'
+ * http://www.cnblogs.com/Answer1215/p/4312265.html
+ * The source map file will only be downloaded if you have source maps enabled and your dev tools open.
+ *
+ * OccurrenceOrderPlugin
+ * Assign the module and chunk ids by occurrence count. Ids that are used often get lower (shorter) ids.
+ * This make ids predictable, reduces to total file size and is recommended.
+ *
+ * UglifyJsPlugin
+ * Minimize all JavaScript output of chunks. Loaders are switched into minimizing mode.
+ *    - 'compress'
+ *      Compressor is a tree transformer which reduces the code size by applying various optimizations on the AST.
+ *
+ * 'NODE_ENV'
+ * React relies on process.env.NODE_ENV based optimizations.
+ * If we force it to production, React will get in an optimized manner.
+ * This will disable some checks (eg. property type checks) and give you a smaller build and improved performance.
+ *    Note: That JSON.stringify is needed as webpack will perform string replace "as is".
+ *    In this case we'll want to end up with strings as that's what various comparisons expect, not just production.
+ *    Latter would just cause an error.
+ *
+ * 'babel'
+ * Babel enables the use of ES6 today by transpiling your ES6 JavaScript into equivalent ES5 source
+ * that is actually delivered to the end user browser.
+ */
 
+/* eslint-disable no-var */
 var webpack = require('webpack');
-var config = require('./webpack.config.base.js');
+var path = require('path');
 
-var SaveAssetsJson = require('assets-webpack-plugin');
-
-config.bail = true;
-config.debug = false;
-config.profile = false;
-config.devtool = '#source-map';
-
-config.output = {
-  path: './client/dist',
-  pathInfo: true,
-  publicPath: '/client/dist/',
-  filename: 'bundle.[hash].min.js'
+module.exports = {
+  entry: './scripts/index',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['', '.js']
+  },
+  devtool: 'source-map',
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'scripts')
+      }
+    ]
+  }
 };
-
-config.plugins = config.plugins.concat([
-  new webpack.optimize.OccurenceOrderPlugin(true),
-  new webpack.optimize.DedupePlugin(),
-  new webpack.optimize.UglifyJsPlugin({
-    output: {
-      comments: false
-    },
-    compress: {
-      warnings: false,
-      screw_ie8: true
-    }
-  }),
-  new SaveAssetsJson({
-    path: process.cwd(),
-    filename: 'assets.json'
-  }),
-  new webpack.DefinePlugin({
-    'process.env': {
-      NODE_ENV: JSON.stringify('production')
-    }
-  })
-]);
-
-config.module.loaders = config.module.loaders.concat([
-  {test: /\.jsx?$/, loaders: [ 'babel'], exclude: /node_modules/}
-]);
-
-module.exports = config;
